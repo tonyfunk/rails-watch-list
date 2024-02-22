aee09e345d69a77e26d6dbd06edccd33d8e888b2
antoine lebeau <antoine.lebeau@gmail.com>
2024-02-22 11:25:31 +0100

    db setup

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id }
+end
